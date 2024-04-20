941c718a84df51110a09350afa2a1c5b40089b72
Jared Hobson <jaredmhobson@gmail.com>
2024-04-20 13:33:24 -0700

    Complete US2 solo features and tests for merchant coupons new page

--- app/controllers/merchant_coupons_controller.rb
+++ app/controllers/merchant_coupons_controller.rb
@@ -1,6 +1,35 @@
 class MerchantCouponsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @coupons = @merchant.coupons
+    @enabled_coupons = @merchant.coupons.enabled
+    @disabled_coupons = @merchant.coupons.disabled
+  end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+
+    coupon = @merchant.coupons.new(coupon_params)
+
+    if coupon.save
+      redirect_to merchant_coupons_path(@merchant)
+    else
+      flash[:alert] = "Error: #{coupon.errors.full_messages.to_sentence}"
+
+      redirect_to new_merchant_coupon_path(@merchant)
+    end
+  end
+
+  private
+  def coupon_params
+    params.permit(
+      :name,
+      :code,
+      :amount,
+      :coupon_type
+    )
   end
 end
--- spec/features/merchant_coupons/index_spec.rb
+++ spec/features/merchant_coupons/index_spec.rb
@@ -16,13 +16,27 @@ RSpec.describe 'Merchant Coupons Index' do
         expect(page).to have_link(coupon.name, href: merchant_coupon_path(merchant1, coupon))
 
         if coupon.coupon_type == "percent"
-          expect(page).to have_content("#{coupon.amount}% off")
+          expect(page).to have_content("Amount: #{coupon.amount}% off")
         else
-          expect(page).to have_content("$#{coupon.amount / 100.0} off")
+          expect(page).to have_content("Amount: $#{(coupon.amount / 100.0).round(2)} off")
         end
       end
 
       expect(page).to_not have_content(coupon2.name)
+    end
+  end
+
+  describe 'User Story 2 Solo' do
+    it 'lists all of my coupon names including their amount off and each coupons name is also a link to its show page' do
+      merchant1 = create(:merchant)
+
+      visit merchant_coupons_path(merchant1)
+
+      expect(page).to have_link("Create new coupon", href: new_merchant_coupon_path(merchant1))
+
+      click_link("Create new coupon")
+
+      expect(current_path).to eq(new_merchant_coupon_path(merchant1))
     end
   end
 end
